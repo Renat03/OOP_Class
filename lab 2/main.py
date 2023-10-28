5c7e325ee7eb0285196c770fc064d1c8e2779428
Renat03 <grsrenat@gmail.com>
2023-10-28 23:48:04 +0300

    Add detection for added and removed files

--- lab 2/main.py
+++ lab 2/main.py
@@ -9,6 +9,7 @@ class FolderMonitor:
     def __init__(self, folder_path):
         self.folder_path = folder_path
         self.documents = self.load_documents()
+        self.committed_documents = self.documents.copy()
         self.last_snapshot_time = None
 
     def load_documents(self):
@@ -19,28 +20,56 @@ class FolderMonitor:
                 ext = os.path.splitext(filename)[1]
                 if ext in ['.txt']:
                     docs.append(TextDocument(file_path))
-                elif ext in ['.png', '.jpg', 'jpeg']:
+                elif ext in ['.png', '.jpg', '.jpeg']:
                     docs.append(ImageDocument(file_path))
                 elif ext in ['.py', '.java']:
                     docs.append(ProgramDocument(file_path))
         return docs
 
     def commit(self):
-        for doc in self.documents:
-            doc.commit()
         self.last_snapshot_time = datetime.datetime.now()
+        self.committed_documents = self.documents.copy()
 
     def status(self):
+        current_files = set(os.listdir(self.folder_path))
+        last_snapshot_files = {doc.filename.split('\\')[-1] for doc in self.committed_documents}
+
+        added_files = set(current_files - last_snapshot_files)
+        removed_files = set(last_snapshot_files - current_files)
+
+        if added_files:
+            for filename in added_files:
+                file_path = os.path.join(self.folder_path, filename)
+                ext = os.path.splitext(filename)[1]
+                if ext in ['.txt']:
+                    self.documents.append(TextDocument(file_path))
+                elif ext in ['.png', '.jpg', '.jpeg']:
+                    self.documents.append(ImageDocument(file_path))
+                elif ext in ['.py', '.java']:
+                    self.documents.append(ProgramDocument(file_path))
+
+        if removed_files:
+            self.documents = [doc for doc in self.documents if doc.filename.split('\\')[-1] not in removed_files]
+
         print(f"\nCreated Snapshot at: {self.last_snapshot_time}") if self.last_snapshot_time else print("\nNo snapshots taken yet.")
+
+        for name in added_files:
+            print(f"{name} - Added")
+        for name in removed_files:
+            print(f"{name} - Removed")
         for doc in self.documents:
-            status = 'Changed' if doc.has_changed() else 'No Change'
             name = doc.filename.split('\\')[-1]
-            print(f"{name} - {status}")
+            if name not in added_files:
+                current_updated_at = datetime.datetime.fromtimestamp(os.path.getmtime(doc.filename))
+                status = 'Changed' if current_updated_at > doc.updated_at else 'No Change'
+                doc.updated_at = current_updated_at
+                print(f"{name} - {status}")
         print('')
 
     def info(self, filename):
         for doc in self.documents:
             if doc.filename.split('\\')[-1] == filename:
+                doc.updated_at = datetime.datetime.fromtimestamp(os.path.getmtime(doc.filename))
                 print(doc.info())
                 break
 
@@ -62,3 +91,4 @@ class FolderMonitor:
 if __name__ == '__main__':
     monitor = FolderMonitor(r"C:\Users\PС\Desktop\oop\lab 2\files")
     monitor.start()
+
--- lab 2/models/document.py
+++ lab 2/models/document.py
@@ -7,7 +7,6 @@ class Document:
         self.extension = os.path.splitext(filename)[1]
         self.created_at = datetime.fromtimestamp(os.path.getctime(filename))
         self.updated_at = datetime.fromtimestamp(os.path.getmtime(filename))
-        self.snapshot_time = None
 
     def info(self):
         return (f'''
@@ -16,13 +15,3 @@ Extension: {self.extension}
 Created time: {self.created_at}
 Updated time: {self.updated_at}
 ''')
-
-    def has_changed(self):
-        if not self.snapshot_time:
-            return False
-        elif self.updated_at > self.snapshot_time:
-            return True
-        return False
-
-    def commit(self):
-        self.snapshot_time = datetime.now()
