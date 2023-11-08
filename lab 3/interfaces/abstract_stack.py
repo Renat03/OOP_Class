06998c94dbd47370b132bc5fc93586ee8459c182
Renat03 <grsrenat@gmail.com>
2023-11-08 10:06:11 +0200

    Add interfaces for the abstract Stack and the abstract Queue

---
+++ lab 3/interfaces/abstract_stack.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+
+class AbstractStack(ABC):
+    @abstractmethod
+    def push(self, item):
+        pass
+
+    @abstractmethod
+    def pop(self):
+        pass
+
+    @abstractmethod
+    def peek(self):
+        pass
+
+    @abstractmethod
+    def is_empty(self):
+        pass
+
+    @abstractmethod
+    def is_full(self):
+        pass
+
